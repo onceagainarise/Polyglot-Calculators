279acd6d82602a8479f90b42a0eb2fe11caa0133
AlexandrAnatoliev <per-1986@list.ru>
2025-11-19 17:08:56 +0300

    fix: solved issue-150

---
+++ Science & Math/Base Converters/BaseConverterCLI-Java-AlexandrAnatoliev/src/Bases.java
@@ -0,0 +1,14 @@
+/**
+* Enum representation supported numeral system bases.
+* Uses for number conversion between different numeral systems.
+*
+* @version  0.1.0
+* @since    19.11.2025
+* @author   AlexandrAnatoliev
+*/
+public enum Bases {
+    BIN,
+    OCT,
+    DEC,
+    HEX
+}
